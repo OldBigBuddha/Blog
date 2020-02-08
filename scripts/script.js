f8069fb6fd7883bcde8a5c63049baca68b3ea5ae
OldBigBuddha <development@oldbigbuddha.net>
2020-02-08 15:47:08 +0900

    [add] script to generate underscore files

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+hexo.extend.generator.register('netlify-headers', locals => {
+  var fs = require('hexo-fs');
+  var pathFn = require('path');
+  const data = pathFn.join( process.env.PWD || process.cwd() , '../underscore/_headers');
+  console.log("Generated: _headers");
+  return {
+    path: "_headers",
+    data: data
+  };
+});
+
+hexo.extend.generator.register('netlify-redirects', locals => {
+  const fs = require('hexo-fs');
+  const pathFn = require('path');
+  const data = pathFn.join( process.env.PWD || process.cwd() , '../underscore/_redirects');
+  console.log("Generated: _redirects");
+  return {
+    path: "_headers",
+    data: data
+  };
+});
